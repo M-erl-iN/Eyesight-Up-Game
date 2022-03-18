a05ba3d64e693130115a2dfd8a00e6b72988de06
M_erl_iN <erlan10122005@gmail.com>
2022-03-18 16:27:08 +0300

    "Style multifunctionality"

---
+++ img/explosions.py
@@ -0,0 +1,5 @@
+colorsS = ((200, 255, 252), (65, 255, 252))
+sprites_to_colorsS = {'FT2.png': colorsS, 'FS_2.png': colorsS}
+button_color1 = 'white'
+COLOR_ACTIVE_MAIN = (255, 255, 255)
+COLOR_INACTIVE_MAIN = (195, 195, 195)

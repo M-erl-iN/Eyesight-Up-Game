e20feadf97b3ef96133b685cc82047261033429b
M_erl_iN <erlan10122005@gmail.com>
2022-03-18 20:38:32 +0300

    Black and Orange Style

---
+++ explosions.py
@@ -0,0 +1,6 @@
+colorsS = ((255, 130, 50), (255, 50, 0))
+sprites_to_colorsS = {'FT2.png': colorsS, 'FS_2.png': colorsS}
+button_color1 = 'black'
+COLOR_ACTIVE_MAIN = (255, 130, 50)
+COLOR_INACTIVE_MAIN = (255, 50, 0)
+RECT_COLOR = (255, 50, 0)

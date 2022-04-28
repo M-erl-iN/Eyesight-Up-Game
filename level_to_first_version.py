3bfec27ecc0930166a84fdf78abedc5a9e81d6c0
M_erl_iN <erlan10122005@gmail.com>
2022-04-28 13:52:12 +0300

    animate buttons, redesign slides and fix dangeons

--- level_to_first_version.py
+++
@@ -1,6 +0,0 @@
-import csv
-
-with open('level.csv', 'w', newline='', encoding='utf8') as csvfile:
-    writer = csv.writer(
-        csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    writer.writerows([(1, 3, 3, 0, 2, 1, 10, 2, 2, 480, 600), (1, 3, 3, 0, 2, 1, 10, 2, 2, 480, 600, 0.5, 2, 2)])
